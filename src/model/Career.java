761ba3e949d79cd8f1cbd463c2fcc603ff758eea
Ivan <ivanloga.dev@gmail.com>
2024-02-22 13:21:57 +0100

    Connector jdbc y ddl

---
+++ src/model/Career.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Career {
+	private int id;
+	private String name;
+	
+	private int getId() {
+		return id;
+	}
+	private void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
